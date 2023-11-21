b2c1a364aa120f38b0b35bbfbd14142fa8cd28d8
eti kona <etikonapal@gmail.com>
2023-11-21 20:15:17 +0600

    experience header completed

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+
+import "react-vertical-timeline-component/style.min.css";
+import { styles } from "../styles";
+import { experiences } from "../constants";
+
+import { textVariant } from "../utils/motion";
+import SectionWrapper from "../hoc/SectionWrapper";
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I done so far</p>
+        <h2 className={styles.sectionHeadText}>Working Experience</h2>
+      </motion.div>
+    </>
+  );
+};
 
-export default Experience
+export default SectionWrapper(Experience, "work");
